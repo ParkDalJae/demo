9f0eda393b3c411e9e043b0c9c99ac60e65e8f59
ParkDalJae <dv.over931@gmail.com>
2023-07-27 14:42:59 +0900

    RequestMapping -> GetMapping으로 변경

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,15 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String home(Model model) {
-		System.out.println("home controller start");
+		System.out.println("home controller start!");
 		
 		model.addAttribute("name", "UserController");
         return "index";
